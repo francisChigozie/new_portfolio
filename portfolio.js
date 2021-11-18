3bff02eef0fa6f098585d99ab4ca0041fa673130
chigozie Francis <franksoft1@yahoo.com>
2021-11-18 18:06:32 +0100

    add cor

--- portfolio.js
+++ portfolio.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs');
 
 app.use(cors({
-    origin: "http://localhost:3000, https://frankfurtfintek.netlify.app/" //or your netlify domain
+    origin: "https://frankfurtfintek.netlify.app/" //or your netlify domain
 }))
 
 app.use(express.urlencoded({extended: true}))
